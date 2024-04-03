0a17ba6405dc9b7d074d69bf67163b4787e0441c
BeepBoopBit <renzaguirre.w@gmail.com>
2024-04-04 02:42:02 +0800

    Remove offset encoding error when going to definition

--- after/plugin/lsp-zero.lua
+++ after/plugin/lsp-zero.lua
@@ -14,6 +14,14 @@ require('mason-lspconfig').setup({
     lua_ls = function()
       local lua_opts = lsp_zero.nvim_lua_ls()
       require('lspconfig').lua_ls.setup(lua_opts)
+      require('lspconfig').clangd.setup{
+          on_attach = on_attach,
+          capabilities = require('cmp_nvim_lsp').default_capabilities(),
+          cmd = {
+              "clangd",
+              "--offset-encoding=utf-16"
+          }
+      }
     end,
   }
 })
